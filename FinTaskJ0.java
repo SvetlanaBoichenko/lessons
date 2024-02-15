d499a5f01e23fe7a2a11b7666511d510c71f7887
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-02-15 20:14:10 +0300

    Add files via upload

---
+++ FinTaskJ0.java
@@ -0,0 +1,38 @@
+public class MainClass {    
+  static boolean sub_string (String s1, String s2)
+  {   
+    boolean ret = false;
+
+    if (s1.length() < s2.length())  
+      return ret;
+  
+      for (int i = 0; i < s1.length(); i++) {
+       if (ret == true) 
+          return ret;
+      
+       if (s1.charAt(i) == s2.charAt(0)) { 
+         ret = true;        
+         
+         for (int j = 1; j < s2.length(); j++) {
+           if (s2.charAt(j) == s1.charAt(i+1))                
+             i++;          
+           else { 
+             ret = false;
+             break;
+            }           
+          }         
+        }      
+      }
+   return ret;    
+  }
+      
+  public static void main(String[] args) {
+    String s1 = "Мой свет - СССвета!";
+    String s2 = "Мой свет ";
+    
+    if(sub_string (s1,s2))
+       System.out.println("Строка 1 содeржит в себе строку 2");
+    else
+      System.out.println("Строка 1 не содeржит строку2 или ее длина меньше длины строки 2");    
+  }
+}
