4eadda8d7203ede588c8321ff74f1930bdb1b7e4
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-02-16 11:32:57 +0300

    Add files via upload

--- FinTaskJ0.java
+++ FinTaskJ0.java
@@ -10,24 +10,26 @@ public class MainClass {
       for (int i = 0; i < s1.length(); i++) {
        if (ret) 
           return true;
-      
-       if (s1.charAt(i) == s2.charAt(0)) { 
+          
          ret = true;        
  
-         for (int j = 1; j < s2.length(); j++) {
+         for (int j = 0; j < s2.length(); j++) {
+           if((j + i) >=  s1.length())
+            return false;
+            
            if (s2.charAt(j) != s1.charAt(i + j)) {             
              ret = false;
              break;
             }           
           }         
         }      
-      }
+      
     return false;    
   }
       
   public static void main(String[] args) {
-    String s1 = "Мой свет - СССвввета!";
-    String s2 = "Свввет";
+    String s1 = "12345";
+    String s2 = "4545";
     
     if(sub_string (s1,s2))
        System.out.println("Строка 1 содeржит в себе строку 2");
