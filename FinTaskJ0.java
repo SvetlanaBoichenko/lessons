ca709f63de8df04cb02a6bd786b3af58ab0f5e25
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-02-16 14:01:28 +0300

    Add files via upload

--- FinTaskJ0.java
+++ FinTaskJ0.java
@@ -1,33 +1,22 @@
 public class MainClass {    
   static boolean sub_string (String s1, String s2)
   {   
-   
-    if (s1.length() < s2.length())  
-      return false;
-  
-      boolean ret = false;
-
-      for (int i = 0; i <= s1.length()- s2.length(); i++) {
-         ret = true;        
- 
-         for (int j = 0; j < s2.length(); j++) {
-
-           if (s2.charAt(j) != s1.charAt(i + j)) {             
-             ret = false;
+    
+      for (int i = 0; i <= s1.length() - s2.length(); i++) {      
+         for (int j = 0; j < s2.length(); j++) { 
+           if (s2.charAt(j) != s1.charAt(i + j))  
              break;
+             
+            if ((j) == s2.length()-1)
+              return true;
             }                      
-          }
-         if (ret) 
-           return true;
-
-        }      
-      
+        }            
     return false;    
   }
       
   public static void main(String[] args) {
-    String s1 = "12345";
-    String s2 = "1";
+    String s1 = "12344455";
+    String s2 = "44";
     
     if(sub_string (s1,s2))
        System.out.println("Строка 1 содeржит в себе строку 2");
