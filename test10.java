912280332a0aeffd1edc91c7b5727d07930b740d
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-03-21 02:34:05 +0300

    Update test10.java

--- test10.java
+++ test10.java
@@ -3,13 +3,13 @@ import ports.ComPort1;
 
 public class test10 {
 
-  public static void main(String[] args){
-    String Buf1 = "#B9\r"; 
+public static void main(String[] args){
+	String Buf1 = "#B9\r"; 
 	String Buf2 = ComPort1.ReadPort(1);
 	
-    int devaddr1 = MyAdam.GetAddr(Buf1);
-    int devaddr2 = ComPort1.GetDevAddr (Buf2);   
+	int devaddr1 = MyAdam.GetAddr(Buf1);
+    	int devaddr2 = ComPort1.GetDevAddr(Buf2);   
 
-	   System.out.print(devaddr1 + "  " + devaddr2 );
+	System.out.print(devaddr1 + "  " + devaddr2 );
   }
 }
