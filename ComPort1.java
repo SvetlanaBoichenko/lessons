8e1797fd76f39d4e3ae92acfbac142e7ccd5c538
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-03-21 02:32:15 +0300

    Update ComPort1.java

--- ComPort1.java
+++ ComPort1.java
@@ -7,23 +7,19 @@ public class ComPort1 {
 	int FDevAddr;
 	String FReadBuf;
 	
-    static MyAdam protAdam = new MyAdam();
+       static MyAdam protAdam = new MyAdam();
 	
 	public ComPort1(int speed) {
-		FSpeed = speed; 
-        
+		FSpeed = speed;  
 	};
 
 	public static String ReadPort(int nport){
-     String retbuf = "#A9\r";     
-	 return retbuf;
-
+        	String retbuf = "#A9\r";     
+	 	return retbuf;
     } 
 	
 	public static int GetDevAddr (String buf) {
-       return  protAdam.GetAddr(buf);     
-
-
+       		return  protAdam.GetAddr(buf);     
 	} 
 	
 } 
