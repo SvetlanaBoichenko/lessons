e4235d5e50037f2691eca3c046ad040c6c1258ea
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-05-20 09:15:51 +0300

    Update ListMap2.java

--- ListMap2.java
+++ ListMap2.java
@@ -4,24 +4,25 @@ import java.util.Random;
 
 public class ListMap2 {
     static ArrayList<Integer> NumRepite (ArrayList<Integer> nList1, int ntimes) {
-        ArrayList<Integer> ResList = new ArrayList<>(10);
-        HashMap<Integer, Integer> map = new HashMap<>(10 );  // Создали мар
+        ArrayList<Integer> ResList = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();  // Создали мар
 
         for (int j = 0; j <= 99; j++) {         //список содерж 1-10, что соотв ключам, значения - счетчики
             int mkey = nList1.get (j);         // Знач в диапазоне 1-10 из листа - это будет ключом
 
             if (map.containsKey (mkey)) {
-                int chet = map.get(mkey);        //Вытащить знач(счетчик), соотв ключу                
+                int chet = map.get(mkey);        //Вытащить знач(счетчик), соотв ключу
+                //Ключ - знач из листа, ind - счетчик
                 chet++;
-                map.put(mkey, chet);             // увелич и меняем счетч в map
+                map.put(mkey, chet); // увелич и меняем счетч
 
                 if (chet > ntimes) {
-                    if (!ResList.contains (mkey)) {
+                    if (!ResList.contains(mkey)) {
                         ResList.add(mkey);
                 }
             }
             else
-                map.put(mkey, 1);
+                map.put(mkey, 1);}
         }
         return  ResList;
     }
